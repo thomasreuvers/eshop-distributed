9c505da1931efeaf4863808fec16c042a845d9d3
Thomas <thomasreuvers1@gmail.com>
2025-07-21 23:23:44 +0200

    Section 6

---
+++ Catalog/Endpoints/ProductEndpoints.cs
@@ -0,0 +1,72 @@
+﻿using Catalog.Domain;
+using Catalog.Services;
+
+namespace Catalog.Endpoints;
+
+public static class ProductEndpoints
+{
+    public static void MapProductEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/products");
+        
+        // GET all
+        group.MapGet("/", async (ProductService service) =>
+        {
+            var products = await service.GetProductsAsync();
+            return Results.Ok(products);
+        })
+        .WithName("GetAllProducts")
+        .Produces<List<Product>>();
+        
+        // GET by id
+        group.MapGet("/{id:Guid}", async (Guid id, ProductService service) =>
+        {
+            var product = await service.GetProductByIdAsync(id);
+            return product is not null ? Results.Ok(product) : Results.NotFound();
+        })
+        .WithName("GetProductById")
+        .Produces<Product>()
+        .Produces(StatusCodes.Status404NotFound);
+        
+        // POST create
+        group.MapPost("/", async (Product product, ProductService service) =>
+            {
+                await service.CreateProductAsync(product);
+                return Results.Created($"/products/{product.Id}", product);
+            })
+            .WithName("CreateProduct")
+            .Produces<Product>(StatusCodes.Status201Created);
+        
+        // PUT update
+        group.MapPut("/{id:Guid}", async (Guid id, Product inputProduct, ProductService service) =>
+        {
+            var product = await service.GetProductByIdAsync(id);
+            if (product is null)
+            {
+                return Results.NotFound();
+            }
+
+            await service.UpdateProductAsync(product, inputProduct);
+            return Results.NoContent();
+        })
+        .WithName("UpdateProduct")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound);
+        
+        // DELETE
+        group.MapDelete("/{id:guid}", async (Guid id, ProductService service) =>
+        {
+            var product = await service.GetProductByIdAsync(id);
+            if (product is null)
+            {
+                return Results.NotFound();
+            }
+
+            await service.DeleteProductAsync(product);
+            return Results.NoContent();
+        })
+        .WithName("DeleteProduct")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+}
--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -1,5 +1,7 @@
 using Catalog.Data;
+using Catalog.Endpoints;
 using Catalog.Extensions;
+using Catalog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 builder.AddNpgsqlDbContext<ProductDbContext>(connectionName: "catalogdb");
+builder.Services.AddScoped<ProductService>();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -23,30 +26,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapProductEndpoints();
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
